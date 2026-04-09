ee3e27865358a59ab0cb637bea5a550595938639
MohammadSaniya5 <saniyamohammad0567@gmail.com>
2026-04-09 18:03:09 +0530

    db modified

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// Test connection
+db.getConnection((err, connection) => {
   if (err) {
-    console.log("❌ Database connection failed", err);
+    console.log("❌ Database connection failed:", err);
   } else {
     console.log("✅ Connected to Railway MySQL database");
+    connection.release(); // VERY IMPORTANT
   }
 });
 
